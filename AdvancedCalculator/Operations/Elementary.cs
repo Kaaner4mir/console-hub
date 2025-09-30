fc7b51c9a6e82ade0370a4bc830abcf712066619
Kaaner <kaaner4mir@gmail.com>
2025-09-30 22:24:00 +0300

    feat: add elementary operations
    Implemented the BasicOperation method in the Elementary class to perform
    basic arithmetic operations based on user input.
    
    Division by zero and 0/0 cases are now validated and display appropriate
    messages instead of causing errors. The result is shown using the ShowResult
    method with symbols for each operation.

---
+++ AdvancedCalculator/Operations/Elementary.cs
@@ -0,0 +1,52 @@
+﻿class Elementary
+{
+    public static double? BasicOperation(string operationName, Func<double, double, double> operation)
+    {
+        try
+        {
+            double val1 = ConsoleManager.GetInput<double>("\n➡️ Enter the first number: ");
+            double val2 = ConsoleManager.GetInput<double>("➡️ Enter the second number: ");
+
+            string opNameLower = operationName.ToLower();
+
+            if (opNameLower == "division")
+            {
+                if (val1 == 0 && val2 == 0)
+                {
+                    ConsoleManager.WriteColored("\n⛔ Undefined!", ConsoleColor.Red);
+                    return null;
+                }
+
+                if (val2 == 0)
+                {
+                    ConsoleManager.WriteColored("\n⛔ The divisor cannot be zero!", ConsoleColor.Red);
+                    return null;
+                }
+            }
+
+            double result = operation(val1, val2);
+
+            return ShowResult(result, val1, val2, opNameLower);
+        }
+        catch (Exception exc)
+        {
+            ConsoleManager.WriteColored($"\n⚠️ An error occurred: {exc.Message}", ConsoleColor.Red);
+            return null;
+        }
+    }
+
+    public static double ShowResult(double result, double val1, double val2, string operationName)
+    {
+        string symbol = operationName switch
+        {
+            "addition" => "+",
+            "subtraction" => "-",
+            "multiplication" => "x",
+            "division" => "/",
+            _ => "?"
+        };
+
+        ConsoleManager.WriteColored($"\n✅ {val1} {symbol} {val2} = {result}", ConsoleColor.Green);
+        return result;
+    }
+}

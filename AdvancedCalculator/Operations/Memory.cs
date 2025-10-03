85e60f094da1e90aa02498649521d608f8961535
Kaaner <kaaner4mir@gmail.com>
2025-10-03 11:05:30 +0300

    feat: add memory operations
    Implemented the memory operations method in the memory class to perform
    memory operations based on user input
    Implemented error conditions in user prompt

---
+++ AdvancedCalculator/Operations/Memory.cs
@@ -0,0 +1,37 @@
+﻿class Memory
+{
+    public static void MemoryOperations()
+    {
+        Console.Clear();
+
+        Menu.MemoryMenu();
+
+        short input = ConsoleManager.GetInput<short>("\n👉 Please enter the operation you wish to perform numerically: ");
+
+        double memory = 0;
+
+        switch (input)
+        {
+            case 1:
+                {
+                    double inputMemory = ConsoleManager.GetInput<double>("\n👉 Enter the number you want to add to memory: ");
+                    memory += inputMemory;
+                    ConsoleManager.WriteColored($"\n💾 Memory: {memory}", ConsoleColor.Green);
+                }
+                break;
+            case 2:
+                {
+                    double inputMemory = ConsoleManager.GetInput<double>("\n👉 Enter the number you want to substract from memory: ");
+                    memory += inputMemory;
+                    ConsoleManager.WriteColored($"\n💾 Memory: {memory}", ConsoleColor.Green);
+
+                }
+                break;
+            case 3: memory = 0; ConsoleManager.WriteColored($"\n💾 Memory: {memory}", ConsoleColor.Green); break;
+            case 4: ConsoleManager.WriteColored($"\n💾 Memory: {memory}", ConsoleColor.Green); break;
+            default:
+                ConsoleManager.WriteColored(
+                "\n❓ The operation you want to perform could not be found.", ConsoleColor.Yellow); break;
+        }
+    }
+}
